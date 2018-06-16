aa0925a792cd749ea35c33aef80dad3aac47c907
Noah Garner <noahguy16@gmail.com>
2018-06-16 16:26:29 -0700

    changed main again

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main()
     int min = 1, max = 1;
     string userExpress = "";
       do {
-        cout << "Welcome the RANDOM ART Creator" << endl
+        cout << "Welcome the random art Creator" << endl
              << "========================================" << endl
              << "1. Enter an Expression" << endl
              << "2. Set min and max recursion depth" << endl
